c3d334cec8a343f7a5e45e57b10cebc0fe1de213
Stephan Petersen <54240006+Stephan-Pe@users.noreply.github.com>
2021-09-16 17:54:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value }); // arrow function binds this to context
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className='App'>
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
