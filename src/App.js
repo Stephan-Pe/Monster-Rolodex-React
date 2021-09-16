7a446f0608fec38cbb30f475b64adf2eb690e8cc
Stephan Petersen <54240006+Stephan-Pe@users.noreply.github.com>
2021-09-16 18:17:57 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -29,8 +29,9 @@ class App extends Component {
     );
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
-          placeholder='Search Monsters'
+          placeholder='Search Monsters...'
           handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
