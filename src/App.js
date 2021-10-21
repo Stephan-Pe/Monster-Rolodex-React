9021e785de20042b8548ca1d0b4f13decbe6bb5e
Stephan Petersen <54240006+Stephan-Pe@users.noreply.github.com>
2021-10-21 15:35:14 +0200

    update packages to latest version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import "./App.css";
+
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -13,28 +15,27 @@ class App extends Component {
     };
   }
 
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value }); // arrow function binds this to context
-  };
-
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
       <div className='App'>
         <h1>Monsters Rolodex</h1>
-        <SearchBox
-          placeholder='Search Monsters...'
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters}></CardList>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import "./search-box.styles.css";
+import React from 'react';
 
-export const SearchBox = ({ placeholder, handleChange }) => (
+import './search-box.styles.css';
+
+export const SearchBox = props => (
   <input
-    className='search'
+    className='search-box'
     type='search'
-    placeholder={placeholder}
-    onChange={handleChange}
+    placeholder='search monsters'
+    onChange={props.onSearchChange}
   />
 );
